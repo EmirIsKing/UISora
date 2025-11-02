f20950fb061a257bdd872a4e769a005ef0416456
EmirIsKing <emirstormzy2@gmail.com>
2025-11-02 23:45:17 +0000

    a lot of vercel error fix

---
+++ lib/blogData.ts
@@ -0,0 +1,91 @@
+export const blogData = [
+  {
+    id:1,
+    title: "A detailed something",
+    description: "lorem ipsiund odsvre flanr ewoknt",
+    image: "/mobile-ui-thread-crafter.png",
+    date: Date.now(),
+    category:"Ai",
+    author: "UISora",
+    author_img: "/uisora-black-rounded.png",
+    slug: "number-1"
+  },
+  {
+    id:2,
+    title: "A detailed something",
+    description: "lorem ipsiund odsvre flanr ewoknt",
+    image: "/mobile-ui-thread-crafter.png",
+    date: Date.now(),
+    category:"Ai",
+    author: "UISora",
+    author_img: "/uisora-black-rounded.png",
+    slug: "number-2"
+  },
+  {
+    id:3,
+    title: "A detailed something",
+    description: "lorem ipsiund odsvre flanr ewoknt",
+    image: "/mobile-ui-thread-crafter.png",
+    date: Date.now(),
+    category:"Ai",
+    author: "UISora",
+    author_img: "/uisora-black-rounded.png",
+    slug: "number-3"
+  },
+  {
+    id:4,
+    title: "A detailed something",
+    description: "lorem ipsiund odsvre flanr ewoknt",
+    image: "/mobile-ui-thread-crafter.png",
+    date: Date.now(),
+    category:"Ai",
+    author: "UISora",
+    author_img: "/uisora-black-rounded.png",
+    slug: "number-4"
+  },
+  {
+    id:5,
+    title: "A detailed something",
+    description: "lorem ipsiund odsvre flanr ewoknt",
+    image: "/mobile-ui-thread-crafter.png",
+    date: Date.now(),
+    category:"Ai",
+    author: "UISora",
+    author_img: "/uisora-black-rounded.png",
+    slug: "number-5"
+  },
+  {
+    id:66,
+    title: "A detailed something",
+    description: "lorem ipsiund odsvre flanr ewoknt",
+    image: "/mobile-ui-thread-crafter.png",
+    date: Date.now(),
+    category:"Ai",
+    author: "UISora",
+    author_img: "/uisora-black-rounded.png",
+    slug: "number-6"
+  },
+  {
+    id:7,
+    title: "A detailed something",
+    description: "lorem ipsiund odsvre flanr ewoknt",
+    image: "/mobile-ui-thread-crafter.png",
+    date: Date.now(),
+    category:"Ai",
+    author: "UISora",
+    author_img: "/uisora-black-rounded.png",
+    slug: "number-7"
+  },
+  {
+    id:8,
+    title: "A detailed something",
+    description: "lorem ipsiund odsvre flanr ewoknt",
+    image: "/mobile-ui-thread-crafter.png",
+    date: Date.now(),
+    category:"Ai",
+    author: "UISora",
+    author_img: "/uisora-black-rounded.png",
+    slug: "number-8"
+  },
+
+]

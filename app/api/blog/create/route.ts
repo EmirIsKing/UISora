fa1be61b081b4452dc481e88e971bb060736f099
EmirIsKing <emirstormzy2@gmail.com>
2025-11-05 20:30:14 +0000

    VERCEL ERROR

--- app/api/blog/create/route.ts
+++ app/api/blog/create/route.ts
@@ -6,7 +6,7 @@ export async function POST(req: Request) {
     const body = await req.json();
 
     const supabase = createClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.SUPABASE_URL!,
       process.env.SUPABASE_SERVICE_ROLE_KEY! // must use service role to insert server-side
     );
 

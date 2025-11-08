1dc04714ef279bcfc556ee525b11c8e81a08e757
EmirIsKing <emirstormzy2@gmail.com>
2025-11-08 14:02:42 +0000

    blog changes and add cdn.uisora.com to allowed image sites

--- next.config.ts
+++ next.config.ts
@@ -21,6 +21,10 @@ const nextConfig: NextConfig = {
                 protocol: "https",
                 hostname: "example.com",
             },
+            {
+                protocol: "https",
+                hostname: "cdn.uisora.com",
+            },
         ],
     },
 

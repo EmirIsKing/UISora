97d3a7013a6d732898c17ccc0f498ee98a140be2
EmirIsKing <emirstormzy2@gmail.com>
2025-11-09 20:58:14 +0000

    vercel error fix

--- lib/ga.ts
+++ lib/ga.ts
@@ -2,7 +2,7 @@ export const GA_ID = "G-8VN6NBHB2E"; // same ID
 
 export const pageview = (url: string) => {
   if (typeof window === "undefined") return;
-  (window as any).gtag("config", GA_ID, {
+  (window as Window & typeof globalThis & { gtag: any }).gtag("config", GA_ID, {
     page_path: url,
   });
 };

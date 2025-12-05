b834ac986f187d29e0dc7d1df7fd4bfc662f89e5
EmirIsKing <emirstormzy2@gmail.com>
2025-12-05 19:11:38 +0000

    socials added to footer

--- components/landingPage/Footer.tsx
+++ components/landingPage/Footer.tsx
@@ -1,11 +1,16 @@
 import Logo from "../Logo";
 import Link from "next/link";
+import {Facebook} from 'lucide-react'
+import { FaTiktok } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+
 
 const Footer = () => {
   return (
     <footer className="bg-card/50 border-t border-border py-5" id="footer">
       <div className="container mx-auto px-4">
-        <div className="grid md:grid-cols-4 gap-8 mb-8">
+        <div className="grid md:grid-cols-5 gap-8 mb-8">
           {/* Brand */}
           <div className="col-span-2 md:col-span-1">
             <Link href="/">
@@ -44,7 +49,17 @@ const Footer = () => {
               <li><a href="/cookie-policy" className="hover:text-primary transition-colors">Cookie Policy</a></li>
             </ul>
           </div>
+        {/*Socials*/}
+        <div>
+          <h4 className="font-semibold mb-4">Social</h4>
+          <ul className="space-y-2 text-sm text-muted-foreground">
+            <li><a href="https://web.facebook.com/uisora" target="_blank" className="hover:text-primary transition-colors"><Facebook/></a></li>
+            <li><a href="https://x.com/uisora_com" target="_blank" className="hover:text-primary transition-colors"><FaXTwitter/></a></li>
+            <li><a href="https://www.instagram.com/uisora_com" target="_blank" className="hover:text-primary transition-colors"><FaInstagram/></a></li>
+            <li><a href="https://www.tiktok.com/@uisora_com" target="_blank" className="hover:text-primary transition-colors"><FaTiktok/></a></li>
+          </ul>
         </div>
+      </div>
 
         <div className="border-t border-border pt-8 flex flex-col md:flex-row justify-between items-center gap-4">
           <p className="text-sm text-muted-foreground">

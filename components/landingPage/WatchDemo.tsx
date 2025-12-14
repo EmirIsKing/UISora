fb67848ac786e0d950d60af7bd71ac34ce067dd4
EmirIsKing <emirstormzy2@gmail.com>
2025-12-14 16:51:29 +0000

    view demo link added

--- components/landingPage/WatchDemo.tsx
+++ components/landingPage/WatchDemo.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Button } from '../ui/button'
 import { ArrowDownRight } from 'lucide-react'
+import Link from "next/link";
 
 const WatchDemo = () => {
   return (
-    <Button variant="outline" className='hover:opacity-65 cursor-pointer hover:scale-[0.98]'>
-        Watch Demo
-        <ArrowDownRight className="size-4" />
-    </Button>
+      <Link href="https://youtu.be/7mSMV8g_8mg" target="_blank">
+          <Button variant="outline" className='hover:opacity-65 cursor-pointer hover:scale-[0.98]'>
+              Watch Demo
+              <ArrowDownRight className="size-4" />
+          </Button>
+      </Link>
   )
 }
 

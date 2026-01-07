95d03c49c7f20aade71eccf5481cea2ff7a12f3e
EmirIsKing <emirstormzy2@gmail.com>
2026-01-07 22:01:51 +0000

    vercel error fix

--- components/landingPage/NewHero.tsx
+++ components/landingPage/NewHero.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image"
-import { Star } from "lucide-react"
+//import Image from "next/image"
+//import { Star } from "lucide-react"
 import SignUp from "./SignUp"
 import WatchDemo from "./WatchDemo"
 import Marquee from "./Marquee"
